77202a48393ac6c86f6b2d5ea6239cbfa8069c0c
Student <example@example.com>
2020-02-25 14:11:57 -0800

    add method

--- high-low.cs
+++ high-low.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+public class Program
 {
-  static void Main()
+  public static void Main()
   {
-    // program code goes here
-  }
+    Console.WriteLine("Would you like to play the higher or lower game? (yes/no)");
+    string userInput = Console.ReadLine();
+
+    if ( userInput == "yes" )
+    {
+      Guess();
+    }
+  }  
+
+  public static void Guess()
+  {
+    int max = 100;
+    int guess = max / 2;
+    Console.WriteLine("Is your number higher of lower than " + guess + "? higher/lower/correct");
+    string userInput = Console.ReadLine();
+
+    if ( userInput == "lower" ) 
+    {
+      guess = guess / 2;
+      Console.WriteLine("Is your number higher of lower than " + guess + "? higher/lower/correct");
+      
+    }
+    else if ( userInput == "higher" )
+    {
+      guess = (max + guess) / 2;
+      Console.WriteLine("Is your number higher of lower than " + guess + "? higher/lower/correct");
+    }
+    else if ( userInput == "correct" )
+    {
+      Console.WriteLine("I have guessed your number! goodbye.");
+    }
+    else 
+    {
+      Console.WriteLine("bye");
+    }
+  } 
+
+  
 }
+
+
